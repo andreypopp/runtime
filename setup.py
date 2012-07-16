1299fb55605cdcfc19204ab75c3570fa8d33e17a
Andrey Popp <8mayday@gmail.com>
2012-07-17 00:33:14 +0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+version = '0.1'
+
+setup(
+    name='runtime',
+    version=version,
+    description='app runtime management',
+    author='Andrey Popp',
+    author_email='8mayday@gmail.com',
+    py_modules=['runtime'],
+    test_suite='tests',
+    zip_safe=False)
